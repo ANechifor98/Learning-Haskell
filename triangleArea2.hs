422197f3cf9774174378a47bed9a29cd3f92c9d2
ANechifor98 <55277183+ANechifor98@users.noreply.github.com>
2019-10-02 16:07:11 +0100

    Add files via upload

---
+++ triangleArea2.hs
@@ -0,0 +1,10 @@
+triangleArea :: Float -> Float -> Float -> Float
+triangleArea a b c
+  | a + b >= c && b + c >= a && a + c >= b = (sqrt (s * (s - a) * (s - b) * (s - c)))
+  | otherwise = error "Not a triangle!"
+  where
+    s=(a+b+c)/2
+--triangleArea :: Float -> Float -> Float -> Float
+--triangleArea a b c 
+--sqrt(s * (s - a) * (s - b) * (s - c))
+--          where  s = (a + b + c) / 2
