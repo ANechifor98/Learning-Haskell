bb19e953921c6b0a809520d43cbc3cb6deca5771
ANechifor98 <55277183+ANechifor98@users.noreply.github.com>
2019-11-06 14:36:46 +0000

    Add files via upload

---
+++ addNode.hs
@@ -0,0 +1,9 @@
+data Tree a = Null | Node a (Tree a) (Tree a) deriving (Read, Show)
+
+-- remember, base case (if a Null node is submitted) and then inductive case
+-- v for value, t1 is the left subtree if value is less than the current node, t2 is the right subtree if greater
+addNode :: Ord a => a -> Tree a -> Tree a 
+addNode v Null = Node v Null Null
+addNode v (Node n t1 t2)
+     | v < n           = Node n (addNode v t1) t2
+     | otherwise       = Node n t1 (addNode v t2)
