972f1e2c586d2af55cd248f6e0fca862297aae35
ANechifor98 <55277183+ANechifor98@users.noreply.github.com>
2019-11-09 20:24:53 +0000

    Create mpSort.hs

---
+++ mpSort.hs
@@ -0,0 +1,26 @@
+data Tree a = Null | Node a (Tree a) (Tree a) deriving (Read, Show)
+ 
+-- remember, base case (if a Null node is submitted) and then inductive case
+-- v for value, t1 is the left subtree if value is less than the current node, t2 is the right subtree if greater
+addNode :: Ord a => a -> Tree a -> Tree a 
+addNode v Null = Node v Null Null
+addNode v (Node n t1 t2)
+      | v < n           = Node n (addNode v t1) t2
+      | otherwise       = Node n t1 (addNode v t2)
+ 
+-- recursive function, base case and inductive case
+makeTree :: Ord a => [a] -> Tree a
+makeTree []        = Null
+makeTree (x:xs)    = addNode x (makeTree xs)
+ 
+--inOrder Traversal
+--First, visit all the nodes in the left subtree
+--Then the root node
+--Visit all the nodes in the right subtree
+--recursive, base case and inductive
+inOrder :: Tree a -> [a]
+inOrder Null                 = []
+inOrder (Node n t1 t2)       = (inOrder t1) ++ [n] ++ (inOrder t2)
+ 
+mpSort :: Ord a => [a] -> [a]
+mpSort a = inOrder (makeTree a)
